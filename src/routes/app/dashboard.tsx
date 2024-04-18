c3ab7d5b265ce1eca8e3a2ce31939357e8aea4aa
Igor Nicoletti <igornicoletti@outlook.com>
2024-04-17 22:19:50 -0300

    updated

--- src/routes/app/dashboard.tsx
+++ src/routes/app/dashboard.tsx
@@ -3,14 +3,14 @@ import { DashboardVariants } from "../../styles/variants"
 
 const { dashcontent, dashwrapper, dashtitle, dashguide, dashhcards, dashhcard, dashhinfo, dashvalue, dashdescript } = DashboardVariants();
 
-const dashboardData = [
+const cardData = [
   { id: 1, title: "Pedidos", value: '38', dashdescript: "+20.1% em relação ao dia anterior", },
   { id: 2, title: "Vendas", value: '124', dashdescript: "+18.1% em relação ao mês passado", },
   { id: 3, title: "Cancelados", value: '12', dashdescript: "-8% em relação ao mês passado", },
   { id: 4, title: "Rendimento total", value: 'R$ 1448.60', dashdescript: "+38% em relação ao mês passado", },
 ]
 
-const data = [
+const lineChartData = [
   { data: '01/01', revenue: 1000 },
   { data: '02/02', revenue: 1245 },
   { data: '03/03', revenue: 3500 },
@@ -19,13 +19,14 @@ const data = [
   { data: '06/06', revenue: 1830 },
 ]
 
-const data2 = [
+const pieChartData = [
   { product: 'Pepperoni', amount: 40 },
   { product: 'Mussarrela', amount: 24 },
   { product: 'Pepperonilll', amount: 12 },
   { product: 'Mussum', amount: 8 },
 ]
-const COLORS = ['#8Be9fd', '#f472b6', '#c084fc', '#fde047']
+
+const pieChartColors = ['#8Be9fd', '#f472b6', '#c084fc', '#fde047']
 
 export const DashboardPage = () => {
   return (
@@ -34,7 +35,7 @@ export const DashboardPage = () => {
         <h1 className={dashtitle()}>Dashboard</h1>
         <div className={dashguide()}>
           <ul className={dashhcards()}>
-            {dashboardData.map((data) => (
+            {cardData.map((data) => (
               <li className={dashhcard()} key={data.id}>
                 <p className={dashhinfo()}>{data.title}</p>
                 <p className={dashvalue()}>{data.value}</p>
@@ -46,7 +47,7 @@ export const DashboardPage = () => {
             <div className='2xl:col-span-4 p-4 md:p-6 rounded-md shadow-lg border-b border-in-cyan bg-gradient-to-br from-in-slate'>
               <p className='uppercase font-medium pb-8'>Visão geral</p>
               <ResponsiveContainer width="100%" height={350}>
-                <LineChart data={data}>
+                <LineChart data={lineChartData}>
                   <CartesianGrid stroke={'#44475a'} vertical={false} />
                   <XAxis stroke={'#FFFFFF'} tickLine={false} axisLine={false} dataKey={'data'} dy={16} />
                   <YAxis stroke={'#FFFFFF'} tickLine={false} axisLine={false} tickFormatter={(value: number) => value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} />
@@ -58,19 +59,19 @@ export const DashboardPage = () => {
               <p className='uppercase font-medium pb-8'>Vendas recentes</p>
               <ResponsiveContainer width="100%" height={350}>
                 <PieChart>
-                  <Pie data={data2} dataKey={'amount'} nameKey={'product'} cx={'50%'} cy={'50%'} outerRadius={150} innerRadius={100} strokeWidth={10} stroke={'#2b2d39'} labelLine={false} label={({ cx, cy, midAngle, innerRadius, outerRadius, value, index }) => {
+                  <Pie data={pieChartData} dataKey={'amount'} nameKey={'product'} cx={'50%'} cy={'50%'} outerRadius={150} innerRadius={100} strokeWidth={10} stroke={'#2b2d39'} labelLine={false} label={({ cx, cy, midAngle, innerRadius, outerRadius, value, index }) => {
                     const RADIAN = Math.PI / 180
                     const radius = 12 + innerRadius + (outerRadius - innerRadius)
                     const x = cx + radius * Math.cos(-midAngle * RADIAN)
                     const y = cy + radius * Math.sin(-midAngle * RADIAN)
                     return <text fill={'#FFFFFF'} x={x} y={y} textAnchor={x > cx ? 'start' : 'end'} dominantBaseline={'center'}>
-                      {data2[index].product.length > 12
-                        ? data2[index].product.substring(0, 12).concat('... ')
-                        : data2[index].product}{' '}
+                      {pieChartData[index].product.length > 12
+                        ? pieChartData[index].product.substring(0, 12).concat('... ')
+                        : pieChartData[index].product}{' '}
                       {value}
                     </text>
                   }}>
-                    {data2.map((_, index) => <Cell key={index} fill={COLORS[index % COLORS.length]} />)}
+                    {pieChartData.map((_, index) => <Cell key={index} fill={pieChartColors[index % pieChartColors.length]} />)}
                   </Pie>
                 </PieChart>
               </ResponsiveContainer>
--- src/routes/app/order.tsx
+++ src/routes/app/order.tsx
@@ -22,35 +22,35 @@ export type OrderProps = {
 export const OrderPage = () => {
   const order = useLoaderData() as OrderProps[]
 
-  const [currentOrder, setCurrentOrder] = useState<OrderProps>()
-  const [currentModal, setCurrentModal] = useState<boolean>(false)
+  const [ currentOrder, setCurrentOrder ] = useState<OrderProps>()
+  const [ currentModal, setCurrentModal ] = useState<boolean>( false )
 
-  const intlNumberFormat = new Intl.NumberFormat('pt-br', { currency: 'BRL', style: 'currency' })
+  const intlNumberFormat = new Intl.NumberFormat( 'pt-br', { currency: 'BRL', style: 'currency' } )
 
-  const handleCloseModal = () => setCurrentModal(false)
+  const handleCloseModal = () => setCurrentModal( false )
 
-  const handleOrderDetails = (data: OrderProps) => {
-    setCurrentOrder(order.find((order) => order.id === data.id))
-    setCurrentModal(true)
+  const handleOrderDetails = ( data: OrderProps ) => {
+    setCurrentOrder( order.find( ( order ) => order.id === data.id ) )
+    setCurrentModal( true )
   }
 
   return (
-    <div className={ordercontent()}>
-      <div className={orderwrapper()}>
-        <h1 className={ordertitle()}>Pedidos</h1>
-        <div className={orderguide()}>
-          <div className={orderfilter()}>
-            <Form className={formcontent()}>
-              <div className={formgroup()}>
-                <input className={forminput()} type='filter' id='filter' placeholder=' ' />
-                <label className={formlabel()} htmlFor='filter'>Filtrar cliente</label>
+    <div className={ ordercontent() }>
+      <div className={ orderwrapper() }>
+        <h1 className={ ordertitle() }>Pedidos</h1>
+        <div className={ orderguide() }>
+          <div className={ orderfilter() }>
+            <Form className={ formcontent() }>
+              <div className={ formgroup() }>
+                <input className={ forminput() } type='filter' id='filter' placeholder=' ' />
+                <label className={ formlabel() } htmlFor='filter'>Filtrar cliente</label>
               </div>
             </Form>
           </div>
-          <div className={orderoverflow()}>
-            <table className={ordertable()}>
-              <thead className={orderthead()}>
-                <tr className={orderrow()}>
+          <div className={ orderoverflow() }>
+            <table className={ ordertable() }>
+              <thead className={ orderthead() }>
+                <tr className={ orderrow() }>
                   <th scope='col'>Ver</th>
                   <th scope='col'>Id do pedido</th>
                   <th scope='col'>Realizado há</th>
@@ -60,45 +60,45 @@ export const OrderPage = () => {
                   <th scope='col'>Ações</th>
                 </tr>
               </thead>
-              <tbody className={ordertbody()}>
-                {order.map((data) => (
-                  <tr className={orderrow()} key={data.id}>
+              <tbody className={ ordertbody() }>
+                { order.map( ( data ) => (
+                  <tr className={ orderrow() } key={ data.id }>
                     <td>
-                      <button className={orderaction()} onClick={() => handleOrderDetails(data)}>
-                        <FileSearchIcon className={ordericon()} aria-hidden={true} />
-                        <p className={ordertooltip()}>Detalhes</p>
+                      <button className={ orderaction() } onClick={ () => handleOrderDetails( data ) }>
+                        <FileSearchIcon className={ ordericon() } aria-hidden={ true } />
+                        <p className={ ordertooltip() }>Detalhes</p>
                       </button>
                     </td>
-                    <td>{data.identifier}</td>
-                    <td>{data.min}</td>
+                    <td>{ data.identifier }</td>
+                    <td>{ data.min }</td>
                     <td>
-                      {data.status === 'Pendente' && <span className={orderstatus()}>Pendente</span>}
-                      {data.status === 'Concluído' && <span className={orderstatus({ color: 'finished' })}>Concluído</span>}
-                      {data.status === 'Cancelado' && <span className={orderstatus({ color: 'canceled' })}>Cancelado</span>}
+                      { data.status === 'Pendente' && <span className={ orderstatus() }>Pendente</span> }
+                      { data.status === 'Concluído' && <span className={ orderstatus( { color: 'finished' } ) }>Concluído</span> }
+                      { data.status === 'Cancelado' && <span className={ orderstatus( { color: 'canceled' } ) }>Cancelado</span> }
                     </td>
-                    <td>{data.name}</td>
-                    <td>{intlNumberFormat.format(data.total)}</td>
+                    <td>{ data.name }</td>
+                    <td>{ intlNumberFormat.format( data.total ) }</td>
                     <td>
-                      <div className={ordersteps()}>
-                        <button className={orderaction({ color: 'finished' })}>
-                          <CheckIcon className={ordericon()} aria-hidden={true} />
-                          <p className={ordertooltip({ color: 'finished' })}>Concluir</p>
+                      <div className={ ordersteps() }>
+                        <button className={ orderaction( { color: 'finished' } ) }>
+                          <CheckIcon className={ ordericon() } aria-hidden={ true } />
+                          <p className={ ordertooltip( { color: 'finished' } ) }>Concluir</p>
                         </button>
-                        <button className={orderaction({ color: 'canceled' })}>
-                          <XIcon className={ordericon()} aria-hidden={true} />
-                          <p className={ordertooltip({ color: 'canceled' })}>Cancelar</p>
+                        <button className={ orderaction( { color: 'canceled' } ) }>
+                          <XIcon className={ ordericon() } aria-hidden={ true } />
+                          <p className={ ordertooltip( { color: 'canceled' } ) }>Cancelar</p>
                         </button>
                       </div>
                     </td>
                   </tr>
-                ))}
+                ) ) }
               </tbody>
             </table>
           </div>
-          <PaginationComponent pageIndex={0} totalCount={105} peerPage={10} />
+          <PaginationComponent pageIndex={ 0 } totalCount={ 105 } peerPage={ 10 } />
         </div>
       </div>
-      <ModalComponent currentModal={currentModal} currentOrder={currentOrder} handleCloseModal={handleCloseModal} />
+      <ModalComponent currentModal={ currentModal } currentOrder={ currentOrder } handleCloseModal={ handleCloseModal } />
     </div>
   )
 }
