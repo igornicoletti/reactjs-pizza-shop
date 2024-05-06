0ea6faa5e555740aa3667ec26845eefff0dd12b1
Igor Nicoletti <igornicoletti@outlook.com>
2024-05-06 00:43:45 -0300

    updated

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 const envSchema = z.object({
-  VITE_API_URL: z.string().url().nullish()
+  VITE_API_URL: z.string().nullish()
 })
 
 export const env = envSchema.parse(import.meta.env)
