ea97d395d0ccd98dfe7adc7c70b4d77ba3c7edf6
Igor Nicoletti <igornicoletti@outlook.com>
2024-06-10 20:22:06 -0300

    api interceptor logout

--- src/routes/app/root.tsx
+++ src/routes/app/root.tsx
@@ -1,11 +1,33 @@
-import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
+import { isAxiosError } from 'axios'
+import { Outlet, useNavigate } from 'react-router-dom'
 
+import { api } from '../../lib/axios'
 import { RootVariants } from '../../styles'
 import { HeaderComponent } from '../../components'
 
 const { rootlayout, rootcontainer } = RootVariants()
 
 export const AppPage = () => {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const interceptorId = api.interceptors.response.use(
+      response => response,
+      error => {
+        if (isAxiosError(error)) {
+          const status = error.response?.status
+          const code = error.response?.data.code
+
+          if (status === 401 && code === 'UNAUTHORIZED') {
+            navigate('/signin', { replace: true })
+          }
+        }
+      }
+    )
+    return () => api.interceptors.response.eject(interceptorId)
+  }, [navigate])
+
   return (
     <div className={rootlayout()}>
       <div className={rootcontainer()}>
