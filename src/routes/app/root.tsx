fbad39e11e2f69571ea45da47ae3970e17aad2d3
Igor Nicoletti <igornicoletti@outlook.com>
2024-06-10 21:09:43 -0300

    api interceptor logout

--- src/routes/app/root.tsx
+++ src/routes/app/root.tsx
@@ -12,19 +12,12 @@ export const AppPage = () => {
   const navigate = useNavigate()
 
   useEffect(() => {
-    const interceptorId = api.interceptors.response.use(
-      response => response,
-      error => {
-        if (isAxiosError(error)) {
-          const status = error.response?.status
-          const code = error.response?.data.code
-
-          if (status === 401 && code === 'UNAUTHORIZED') {
-            navigate('/signin', { replace: true })
-          }
-        }
-      }
-    )
+    const interceptorId = api.interceptors.response.use((response) => response, (error) => {
+      isAxiosError(error)
+        && error.response?.status === 401
+        && error.response?.data.code === 'UNAUTHORIZED'
+        && navigate('/signin', { replace: true })
+    })
     return () => api.interceptors.response.eject(interceptorId)
   }, [navigate])
 
